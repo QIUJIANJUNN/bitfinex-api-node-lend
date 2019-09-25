90753f05b7650e7161c756c415a942a79139dcd5
Wayne Chiu <chaoweichiu@gmail.com>
2019-09-25 14:24:45 +0800

    format

--- index.js
+++ index.js
@@ -31,15 +31,18 @@ function get_funding_balance(currency) {
 }
 
 // timestampToTime
-function timestamp_to_time(timestamp){
-    const date = new Date(timestamp)
-    Y = date.getFullYear() + '-';
-    M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
-    D = date.getDate() + ' ';
-    h = date.getHours() + ':';
-    m = date.getMinutes() + ':';
-    s = date.getSeconds(); 
-    return Y+M+D+h+m+s
+function timestamp_to_time(timestamp) {
+  const date = new Date(timestamp);
+  Y = date.getFullYear() + "-";
+  M =
+    (date.getMonth() + 1 < 10
+      ? "0" + (date.getMonth() + 1)
+      : date.getMonth() + 1) + "-";
+  D = date.getDate() + " ";
+  h = date.getHours() + ":";
+  m = date.getMinutes() + ":";
+  s = date.getSeconds();
+  return Y + M + D + h + m + s;
 }
 
 // Check All funding Loans.
